fb49480fc27d7f2264585d1602b4cfe5ceddc605
Emil Gusgård <gusemil@hotmail.com>
2021-01-23 18:09:58 +0200

    enemy movement almost there. Finds the player but moves a bit drunkenly around the player

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -3,17 +3,31 @@ extends KinematicBody
 enum STATE {IDLE, CHASE, ATTACK, DEAD}
 var current_state = STATE.IDLE
 
-var player = null #reference to the player
-
 export var body_removes_after_seconds = 5
-export var sight_cone_degrees = 45.0
+var body_removal_timer : Timer
 
 onready var animation_player = $Graphics/AnimationPlayer
 onready var health_manager = $HealthManager
 
+#Movement
 export var turn_speed_per_second = 360.0
+var movement_vector : Vector3
+export var speed = 300
+#export var friction = 0.875
+export var gravity = 80
+export var move_accel = 4
+var drag = 0.0
+export var max_speed = 25
 
-var body_removal_timer : Timer
+var move_direction = Vector3()
+var velocity = Vector3()
+
+#Navigation and player sensing
+var player = null #reference to the player
+var path = []
+export var sight_cone_degrees = 45.0
+onready var nav : Navigation = get_parent() # enemy must be a child of navigation node
+
 func _ready():
 	player = get_tree().get_nodes_in_group("player")[0] # When multiplayer is implemented take the nearest player object with the tag/group "player"
 	var bone_attachments = $Graphics/Armature/Skeleton.get_children()
@@ -31,6 +45,8 @@ func _ready():
 	body_removal_timer.connect("timeout", self, "remove_body")
 	add_child(body_removal_timer)
 
+	drag = float(move_accel) / max_speed
+
 func _process(delta):
 	#print(has_player_in_line_of_sight())
 	print(can_see_player())
@@ -39,10 +55,18 @@ func _process(delta):
 			set_state(STATE.CHASE)
 		pass
 	elif(current_state == STATE.CHASE):
-		var goal_position = player.global_transform.origin
+		var player_position = player.global_transform.origin
 		var my_position = global_transform.origin
-		var direction = my_position.direction_to(goal_position)
+		path = nav.get_simple_path(my_position, player_position)
+		var goal_position = player_position
 
+		if path.size() > 1: #0 positio on vihollinen oma sijainti
+			goal_position = path[1]
+
+		var direction = goal_position - my_position
+		direction.y = 0
+		set_movement_vector(direction)
+		move(delta)
 		face_direction(direction, delta)
 	elif(current_state == STATE.ATTACK):
 		#state_attack(delta)
@@ -51,12 +75,17 @@ func _process(delta):
 		#print("DEAD")
 		pass
 
+#func _physics_process(delta):
+#	move(delta)
+
 func set_state(state: int):
 	current_state = state
 
-	if(current_state == STATE.IDLE):
+	if current_state == STATE.IDLE:
 		animation_player.play("idle_loop")
-	elif(current_state == STATE.DEAD):
+	elif current_state == STATE.CHASE:
+		animation_player.play("walk_loop", 0.2) #0.2s animation time
+	elif current_state == STATE.DEAD :
 		on_death()
 
 func hurt(damage: int, dir : Vector3): #refactor this. Vector3 is unnecessary
@@ -70,8 +99,7 @@ func state_attack(delta):
 	print("ATTACKING")
 	
 func disable_all_collisions():
-	self.collision_layer = 0
-	self.collision_mask = 0
+	$CollisionShape.disabled = true #Much better collision disabling
 
 func remove_body():
 	print("remove body!")
@@ -80,6 +108,7 @@ func remove_body():
 func on_death():
 	animation_player.play("die")
 	disable_all_collisions()
+	#TODO STOP MOVEMENT
 	body_removal_timer.start() #tarvitaan start obviously
 
 func can_see_player(): #unnecessary probably
@@ -106,4 +135,21 @@ func face_direction(direction: Vector3, delta):
 		rotation.y = atan2(direction.x, direction.z)
 	else:
 		rotation.y += deg2rad(turn_speed_per_second) * delta * turn_right
+
+func set_movement_vector(_movement_vector : Vector3):
+	movement_vector = _movement_vector.normalized()
+
+func move(delta):
+	move_direction = movement_vector
+	move_direction.y = 0
+	move_direction = move_direction.normalized()
+	
+	movement_vector = movement_vector.rotated(Vector3.UP, rotation.y) #Ignores rotation so it follows the player properly
+
+	velocity += move_accel * movement_vector - velocity * Vector3(drag, 0, drag) + gravity * Vector3.DOWN * delta
+	velocity = move_and_slide_with_snap(velocity, Vector3.DOWN, Vector3.UP)
+
+	#velocity += move_direction*speed*delta
+	#print("VELOCITY: ", velocity, " MOVEDIRECTION: ", move_direction)
+	#velocity = move_and_slide_with_snap(velocity, Vector3.DOWN, Vector3.UP)
 
