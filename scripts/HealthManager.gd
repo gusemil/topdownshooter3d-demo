8e1988808116b22efe8614e7e18f92f3c9e2cc40
Emil Gusgård <gusemil@hotmail.com>
2021-01-28 19:16:33 +0200

    infinitely spawning enemies, blood sprays (amount based on damage)

--- scripts/HealthManager.gd
+++ scripts/HealthManager.gd
@@ -9,9 +9,7 @@ signal take_damage
 signal health_changed
 #signal gibbed
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var blood_spray_prefab = preload("res://scenes/BloodSpray.tscn")
 
 func take_damage(dmg : int):
 	current_health -= dmg
@@ -27,4 +25,12 @@ func take_damage(dmg : int):
 	emit_signal("health_changed", current_health)
 	print("damage ", dmg, " taken. Current health: ", current_health)
 
+	spawn_blood_spray(dmg)
 
+func spawn_blood_spray(dmg : int, blood_modifier = 2.0):
+	var blood_spray_instance = blood_spray_prefab.instance()
+	var particles = blood_spray_instance.get_child(0)
+	print(particles.name)
+	particles.set_amount(dmg * blood_modifier)
+	get_tree().get_root().add_child(blood_spray_instance)
+	blood_spray_instance.global_transform.origin = global_transform.origin + Vector3(0,2,0)
--- scripts/WaveManager.gd
+++ scripts/WaveManager.gd
@@ -6,13 +6,16 @@ var navmesh
 
 enum ENEMIES {MELEE, RANGED}
 
-var spawns_per_wave : int = 1
-var enemies_per_spawn : int = 1
-var spawn_timer : float = 1.0
+export var ranged_enemy_chance = 4
+export var spawn_rate : float = 2.5
+#var spawns_per_wave : int = 1
+#var enemies_per_spawn : int = 1
+
 
 var enemy_prefabs = []
 
 var rng = RandomNumberGenerator.new()
+var spawn_timer : Timer
 
 func _ready():
 	rng.randomize()
@@ -20,11 +23,14 @@ func _ready():
 	enemy_prefabs.push_back(melee_enemy_prefab)
 	enemy_prefabs.push_back(ranged_enemy_prefab)
 
-	spawn_random_enemy()
-	spawn_random_enemy()
-	spawn_random_enemy()
+	spawn_timer = Timer.new() #aka ienumerator waitseconds
+	spawn_timer.wait_time = spawn_rate
+	spawn_timer.connect("timeout", self, "spawn_random_normal_enemy")
+	add_child(spawn_timer)
+	spawn_timer.start()
+	spawn_timer.set_one_shot(false)
 
-func spawn_random_enemy():
+func spawn_random_normal_enemy():
 	var direction = rng.randi_range(0,3)
 	var spawn_point : Vector3
 
@@ -38,11 +44,13 @@ func spawn_random_enemy():
 	elif(direction == 3):
 		spawn_point = Vector3(rng.randi_range(40,47),0,rng.randi_range(-47,47))
 
-	var enemy_instance = enemy_prefabs[rand_range(0, enemy_prefabs.size())].instance()
+	var enemy_choice = rng.randi_range(0,ranged_enemy_chance)
+	print(enemy_choice)
+	var enemy_instance
+	if enemy_choice == ranged_enemy_chance:
+		enemy_instance = enemy_prefabs[1].instance()
+	else:
+		enemy_instance = enemy_prefabs[0].instance()
+
 	navmesh.add_child(enemy_instance)
 	enemy_instance.global_transform.origin = spawn_point
-	print(enemy_instance.name)
-
-	#var projectile_instance = projectile_prefab.instance()
-	#get_tree().get_root().add_child(enemy_instance)
-	#projectile_instance.global_transform = global_transform
