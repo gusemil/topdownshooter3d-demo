91f090ddfda108d432e8230b5624f434ccbf267c
Emil Gusgård <gusemil@hotmail.com>
2021-02-17 21:47:52 +0200

    enemy spawning made more interesting and dynamic, game is overall more difficult

--- scripts/WaveManager.gd
+++ scripts/WaveManager.gd
@@ -7,16 +7,31 @@ var navmesh
 enum ENEMIES {MELEE, RANGED}
 
 export var ranged_enemy_chance = 4
-export var spawn_rate : float = 2.5
+export var starting_spawn_rate : float = 2.5
 export var spawning : bool = true
-#var spawns_per_wave : int = 1
-#var enemies_per_spawn : int = 1
+var enemies_per_wave : int = 10
+var enemies_spawned : int = 0
+var wave_delay_time : float = 5
+var spawn_rate_reduction_per_wave : float = 0.25
+var double_spawn_chance : int = 1
+
+const min_spawn_rate : float = 0.5
+const max_double_spawn_chance : int = 10
+const double_spawn_cap : int = 20
 
 
 var enemy_prefabs = []
 
 var rng = RandomNumberGenerator.new()
 var spawn_timer : Timer
+var wave_delay_timer : Timer
+
+var wave_count = 0
+
+var enemy_move_speed_bonus : float = 0
+var enemy_projectile_speed_bonus : float = 0
+var enemy_attack_rate_bonus : float = 0
+var max_enemy_attack_rate : float = 0.75
 
 func _ready():
 	rng.randomize()
@@ -24,12 +39,18 @@ func _ready():
 	enemy_prefabs.push_back(melee_enemy_prefab)
 	enemy_prefabs.push_back(ranged_enemy_prefab)
 
-	spawn_timer = Timer.new() #aka ienumerator waitseconds
-	spawn_timer.wait_time = spawn_rate
+	spawn_timer = Timer.new()
+	spawn_timer.wait_time = starting_spawn_rate
 	spawn_timer.connect("timeout", self, "spawn_random_normal_enemy")
 	add_child(spawn_timer)
 	spawn_timer.start()
 	spawn_timer.set_one_shot(false)
+
+	wave_delay_timer = Timer.new()
+	wave_delay_timer.wait_time = wave_delay_time
+	wave_delay_timer.connect("timeout", self, "start_spawning")
+	add_child(wave_delay_timer)
+	wave_delay_timer.set_one_shot(false)
 
 func spawn_random_normal_enemy():
 	if spawning:
@@ -51,11 +72,56 @@ func spawn_random_normal_enemy():
 		var enemy_instance
 		if enemy_choice == ranged_enemy_chance:
 			enemy_instance = enemy_prefabs[1].instance()
+			enemy_instance.get_node("AttackArea/ProjectileSpawner").projectile_speed += enemy_projectile_speed_bonus
 		else:
 			enemy_instance = enemy_prefabs[0].instance()
+
+		enemy_instance.move_acceleration += enemy_move_speed_bonus
+		enemy_instance.max_speed += enemy_move_speed_bonus
+		enemy_instance.attack_rate -= enemy_attack_rate_bonus
 
 		navmesh.add_child(enemy_instance)
 		enemy_instance.global_transform.origin = spawn_point
 
-func stop_spawning():
+		enemies_spawned += 1
+
+		var double_spawn_check = rng.randi_range(0, double_spawn_cap)
+		if double_spawn_check <= double_spawn_chance:
+			print("DOUBLE SPAWN")
+			spawn_random_normal_enemy()
+
+		if enemies_spawned >= enemies_per_wave:
+			enemies_spawned = 0
+			pause_spawning()
+		
+		print("ENEMIES_SPAWNED: ", enemies_spawned)
+
+func pause_spawning():
 	spawning = false
+	spawn_timer.set_paused(true)
+	wave_delay_timer.start()
+	increase_difficulty()
+
+func start_spawning():
+	print("START SPAWNING")
+	spawning = true
+	spawn_timer.set_paused(false)
+	wave_delay_timer.stop()
+
+func increase_difficulty():
+	enemies_per_wave += 10
+	wave_delay_time += 0.5
+	wave_count += 1
+	enemy_move_speed_bonus += 1
+	enemy_projectile_speed_bonus += 1
+	
+	if spawn_timer.wait_time > min_spawn_rate:
+		spawn_timer.wait_time -= spawn_rate_reduction_per_wave
+	else:
+		if enemy_attack_rate_bonus < max_enemy_attack_rate:
+			enemy_attack_rate_bonus += 0.05
+
+		if double_spawn_chance < max_double_spawn_chance:
+			double_spawn_chance += 1
+		
+
